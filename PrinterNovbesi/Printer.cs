3dd2c8573bfe680b7c7a196ae5740ed2d21aabef
shafiga09@outlook.com <shafiga09@outlook.com>
2023-04-17 02:31:18 +0400

    Add project files.

---
+++ PrinterNovbesi/Printer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+public partial class Document
+        {
+    public class Printer
+    {
+        private Queue<Document> _printQueue = new Queue<Document>();
+
+        public void AddDocumentToQueue(Document document)
+        {
+            _printQueue.Enqueue(document);
+            Console.WriteLine($"{document.Name} added to print queue.");
+        }
+
+        public void PrintNextDocument()
+        {
+            if (_printQueue.Count == 0)
+            {
+                Console.WriteLine("Print queue is empty.");
+                return;
+            }
+
+            var nextDocument = _printQueue.Dequeue();
+            Console.WriteLine($"Printing document: {nextDocument.Name}");
+        }
+
+        public void PrintQueueStatus()
+        {
+            Console.WriteLine($"Documents in print queue: {_printQueue.Count}");
+            if (_printQueue.Count > 0)
+            {
+                Console.WriteLine($"Next document to print: {_printQueue.Peek().Name}");
+            }
+        }
+    }
+
+}
+  
---
+++ PrinterNovbesi/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Document;
+
+namespace PrinterNovbesi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                var printer = new Printer();
+
+                printer.AddDocumentToQueue(new Document("Document 1", 5));
+                printer.AddDocumentToQueue(new Document("Document 2", 2));
+                printer.AddDocumentToQueue(new Document("Document 3", 7));
+
+                printer.PrintQueueStatus(); 
+
+                printer.PrintNextDocument(); 
+
+                printer.PrintQueueStatus(); 
+
+                printer.PrintNextDocument();
+
+                printer.PrintQueueStatus(); 
+
+                printer.PrintNextDocument(); 
+
+                printer.PrintQueueStatus();
+
+                printer.PrintNextDocument(); 
+            }
+        }
+
+    }
+    public partial class Document
+        {
+            public string Name { get; set; }
+            public int PageCount { get; set; }
+
+            public Document(string name, int pageCount)
+            {
+                Name = name;
+                PageCount = pageCount;
+            }
+
+}
+  
